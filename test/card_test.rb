3d8d346bf0bba4748b6c722ec862e2698c1d9699
Brant Wellman <BrantWellman@gmail.com>
2016-07-09 12:47:47 -0600

    Card initialized with question and answer

--- test/card_test.rb
+++ test/card_test.rb
@@ -8,4 +8,16 @@ class CardTest< Minitest::Test
   def test_card_exists
     assert Card
   end
+
+  def test_it_is_initialized_with_question
+    card = Card.new("Where would you rather be?", "Alaska")
+
+    assert_equal "Where would you rather be?", card.question
+  end
+
+  def test_it_is_initialized_with_answer
+    card = Card.new("Where would you rather be?", "Alaska")
+
+    assert_equal "Alaska", card.answer
+  end
 end
